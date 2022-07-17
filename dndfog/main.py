0745d89b8ac62bd8b5ac373862b3adb14e08989f
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2022-07-17 11:06:05 +0300

    Working zoom

--- dndfog/main.py
+++ dndfog/main.py
@@ -6,34 +6,44 @@ import pygame
 
 
 def zoom_at_mouse_pos(
-    display_size: tuple[int, int],
     mouse_position: tuple[int, int],
+    grid_pos: tuple[int, int],
+    old_gridsize: int,
+    new_gridsize: int,
+) -> tuple[int, int]:
+
+    absolute_mouse_position = mouse_position[0] + grid_pos[0], mouse_position[1] + grid_pos[1]
+
+    old_grid_place = absolute_mouse_position[0] / old_gridsize, absolute_mouse_position[1] / old_gridsize
+    new_grid_place = absolute_mouse_position[0] / new_gridsize, absolute_mouse_position[1] / new_gridsize
+
+    camera_delta = (
+        round((new_grid_place[0] - old_grid_place[0]) * new_gridsize),
+        round((new_grid_place[1] - old_grid_place[1]) * new_gridsize),
+    )
+
+    return camera_delta
+
+
+def draw_grid(
+    display: pygame.Surface,
     gridsize: int,
-    change: int,
-) -> tuple[tuple[int, int], tuple[int, int]]:
-
-    new_size = gridsize + change
-    mouse_centered = display_size[0] - mouse_position[0], display_size[1] - mouse_position[1]
-
-    camera_delta = (0, 0)
-
-    return new_size, camera_delta
-
-
-def draw_grid(display: pygame.Surface, gridsize: int, camera: tuple[int, int] = (0, 0)) -> None:
+    camera: tuple[int, int],
+    color: tuple[int, int, int] = (0xC5, 0xC5, 0xC5),
+) -> None:
 
     width, height = display.get_size()
 
-    start_x = ((round(camera[0]) + gridsize - 1) // gridsize) * gridsize - 1
-    start_y = ((round(camera[1]) + gridsize - 1) // gridsize) * gridsize - 1
+    start_x = ((camera[0] + gridsize - 1) // gridsize) * gridsize - 1
+    start_y = ((camera[1] + gridsize - 1) // gridsize) * gridsize - 1
     end_x = start_x + width + gridsize
     end_y = start_y + height + gridsize
 
     for x in range(start_x, end_x, gridsize):
-        pygame.draw.line(display, (0xC5, 0xC5, 0xC5), (x - camera[0], 0), (x - camera[0], height), 2)
+        pygame.draw.line(display, color, (x - camera[0], 0), (x - camera[0], height), 2)
 
     for y in range(start_y, end_y, gridsize):
-        pygame.draw.line(display, (0xC5, 0xC5, 0xC5), (0, y - camera[1]), (width, y - camera[1]), 2)
+        pygame.draw.line(display, color, (0, y - camera[1]), (width, y - camera[1]), 2)
 
 
 def main():
@@ -47,7 +57,7 @@ def main():
     grid_pos = (0, 0)
 
     flags = pygame.SRCALPHA | pygame.RESIZABLE  # | pygame.NOFRAME
-    display = pygame.display.set_mode((1500, 800), flags=flags)
+    display = pygame.display.set_mode((800, 800), flags=flags)
 
     while True:
         mouse_pos = pygame.mouse.get_pos()
@@ -62,14 +72,15 @@ def main():
                 sys.exit()
 
             if event.type == pygame.MOUSEWHEEL:
-                gridsize, camera_delta = zoom_at_mouse_pos(display.get_size(), mouse_pos, gridsize, change=event.y)
-                grid_pos = grid_pos[0] + camera_delta[0], grid_pos[1] + camera_delta[1]
+                old_gridsize = gridsize
+                gridsize = gridsize + event.y
+                camera_delta = zoom_at_mouse_pos(mouse_pos, grid_pos, old_gridsize, gridsize)
+                grid_pos = grid_pos[0] - camera_delta[0], grid_pos[1] - camera_delta[1]
 
             if pressed_buttons[1]:
                 grid_pos = grid_pos[0] - mouse_speed[0], grid_pos[1] - mouse_speed[1]
 
         display.fill((0xCC, 0xCC, 0xCC))
-        pygame.draw.rect(display, (255, 0, 0), (100, 100, 10, 10))
         draw_grid(display, gridsize, grid_pos)
 
         clock.tick(frame_rate)
